a423256c3473e04c1317ef5e5a2c917f72e3a595
Arthur José <57883830+Arthurit0@users.noreply.github.com>
2022-04-28 19:23:06 -0300

    Ex1 do LAB 2 feito, começando EX2

--- Semana 5/Exemplo Fatorial Recursivo.asm
+++ Semana 5/Exemplo Fatorial Recursivo.asm
@@ -13,18 +13,23 @@ end:
 	
 fatorial:
 	# if ($a0 < 1)
-	blt $a0, 1 , fat_parada
+	blt $a0, 1, fat_parada
 	# salvar contexto
-	addi $sp, $sp, 8
+	addi $sp, $sp, -8
 	sw $s0, 0($sp)
 	sw $ra, 4($sp)
-	# código principal com chamada recursiva
+	# c´odigo principal com chamada recursiva
 	move $s0, $a0
 	addi $a0, $a0, -1
-	jal fatorial # $v0 = fatorial ($a0-1)
+	jal fatorial # $v0 = fatorial($a0 -1)
 	mul $v0, $v0, $s0 # $v0 *= $s0
 	# restaurar
+	lw $s0, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	# retornar
 	jr $ra
+
 	
 fat_parada:
 	ori $v0, $zero, 1
